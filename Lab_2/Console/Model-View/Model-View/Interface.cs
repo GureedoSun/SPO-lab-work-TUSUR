98b4d7f34d86d5d913df5e5782db865a48f63467
GureedoSun <Alukard_1996@mail.ru>
2016-03-27 21:34:13 +0600

    Интерфэйс Ifigure

---
+++ Lab_2/Console/Model-View/Model-View/Interface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Интерфейс фигур
+    /// </summary>
+    public interface IFigure
+    {
+        /// <summary>
+        /// Вызов метода рассчёта обьёма
+        /// </summary>
+        /// <returns></returns>
+        double CalculateVolume();
+    }
+}
