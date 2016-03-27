0e47bf3dbc170d068ff898f710e23c7c66de4d9a
GureedoSun <Alukard_1996@mail.ru>
2016-03-27 21:32:23 +0600

    Класс "Шар"

---
+++ Lab_2/Class_VolumeBall/Class_VolumeBall/Class2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_VolumeBall
+{
+    public class ball 
+    {
+        private int _radius; //rarius - раиус шара 
+
+        public int radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+                _radius = value;
+            }
+        }
+        public double CalculateVolume()
+        {
+            return (4 / 3) * Math.PI * (Math.Pow(radius, 3));
+        }
+
+    }
+}
