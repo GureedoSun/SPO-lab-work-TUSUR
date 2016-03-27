2423ddce682320e70e02918a318327ea507ed829
GureedoSun <Alukard_1996@mail.ru>
2016-03-27 21:33:03 +0600

    Класс "Параллепипеда"

---
+++ Lab_2/Class_VolumeParalepipid/Class_VolumeParalepipid/Class3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_VolumeParalepipid
+{
+   
+    public class Parallelepiped
+    {
+        private int _A; // A - ширина
+        private int _B; // B - длинна
+        private int _C; // С - высота 
+        
+        public int A
+        {
+            get
+            {
+                return _A;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+                _A = value;
+            }
+        }
+
+        public int B
+        {
+            get
+            {
+                return _B;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+                _B = value;
+            }
+        }
+        
+        
+        public int C
+        {
+            get
+            {
+                return _C;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+                _C = value;
+            }
+        } 
+            
+        public int CalculateVolume()
+        {
+            return A * B * C;
+        }
+
+
+    }
+
+
+
+}
+
+
